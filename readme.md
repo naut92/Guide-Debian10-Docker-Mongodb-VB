1bd9fd46612b02a4446afbcb3b5b194b4560be4d
naut <naut92@gmail.com>
2019-11-30 20:58:04 +0100

    Initial commit

---
+++ readme.md
@@ -0,0 +1,11 @@
+Dedian 10.2 Baster
+
+image: debian-10.2.0-amd64-netinst.iso
+
+VirtualBox 6.0.14
+
+MongoDB:
+
+$ docker pull mongo:latest
+
+Java 8
---
+++ src/test/java/com/github/naut92/Guide/Debian10/Docker/Mongodb/VB/GuideDebian10DockerMongodbVbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.naut92.Guide.Debian10.Docker.Mongodb.VB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuideDebian10DockerMongodbVbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
